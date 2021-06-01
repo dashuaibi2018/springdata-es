912330d438ee7a33d465bee453489ead22004312
sj <393834461@qq.com>
2021-06-01 17:36:45 +0800

    2

--- src/test/java/com/deji/demo/MerchantTests.java
+++ src/test/java/com/deji/demo/MerchantTests.java
@@ -4,12 +4,13 @@ import com.deji.demo.bean.entity.MerchantSku;
 import com.deji.demo.dao.MerchantSkuRepository;
 import com.deji.demo.service.MerchantSkuService;
 import lombok.extern.slf4j.Slf4j;
-import org.elasticsearch.index.query.BoolQueryBuilder;
-import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.index.query.*;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
@@ -124,6 +125,41 @@ public class MerchantTests {
             System.out.println(x);
         });
 
+    }
+
+    @Test
+    public void queryShould() {
+
+        BoolQueryBuilder queryBuilders = QueryBuilders.boolQuery();
+        queryBuilders.should(QueryBuilders.matchPhraseQuery("title", "springboot"));
+        queryBuilders.should(QueryBuilders.matchPhraseQuery("content", "springboot"));
+
+        System.out.println(queryBuilders.toString());
+        Iterable<MerchantSku> search = merchantDao.search(queryBuilders);
+        search.forEach(x -> {
+            System.out.println(x);
+        });
+
+    }
+
+    /**
+     * @description: 模糊纠错、通配符、前缀查询
+     */
+    @Test
+    public void fuzzyQuery() {
+
+        //fuzziness 即为最多纠正两个字母然后去匹配，默认为 auto（2）
+//        FuzzyQueryBuilder builder = QueryBuilders.fuzzyQuery("sku_name", "一番打");  //模糊纠错查询
+//        WildcardQueryBuilder builder = QueryBuilders.wildcardQuery("sku_name", "艺术");  //通配符查询
+        PrefixQueryBuilder builder = QueryBuilders.prefixQuery("sku_name", "12");  //前缀查询
+        System.out.println(builder);
+
+        Pageable pageable = PageRequest.of(0,10, Sort.Direction.DESC, "createTime");
+        List<MerchantSku> content = merchantDao.search(builder, pageable).getContent();
+
+        content.forEach(
+               a-> System.out.println(a)
+        );
     }
 
 
